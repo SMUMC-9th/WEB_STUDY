f188e5d2113b88750a0c3c1b8fb0b04640ed5374
Jimyeong Kim <kjm021221@icloud.com>
2025-09-22 14:27:09 +0900

    mission: 2주차 미션 1(2)

--- 지미_김지명/2주차_미션/mission1/src/components/Todo.tsx
+++ 지미_김지명/2주차_미션/mission1/src/components/Todo.tsx
@@ -1,79 +1,32 @@
-import React, { useState, type FormEvent } from 'react'
-import type { TTodo } from '../types/todo';
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+import { useTodo } from "../context/TodoContext";
 
-const Todo: React.FC = () => {
-    const [todos, setTodos] = useState<TTodo[]>([]);
-    const [doneTodos, setDoneTodos] = useState<TTodo[]>([]);
-    const [input, setInput] = useState<string>('');
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
-        e.preventDefault();
-        const text = input.trim();
-
-        if(text) {
-            const newTodo: TTodo = { id: Date.now(), text};
-            setTodos((prevTodos): TTodo[] => [...prevTodos, newTodo]);
-            setInput('');
-        }
-    };
-
-    const completeTodo = (todo: TTodo): void => {
-        setTodos(prevTodos => prevTodos.filter((t):boolean => t.id !== todo.id));
-        setDoneTodos(prevDoneTodos => [...prevDoneTodos, todo]);
-    };
-
-    const deleteTodo = (todo: TTodo): void => {
-        setDoneTodos(prevDoneTodos => prevDoneTodos.filter((t):boolean => t.id !== todo.id));
-    };
-
-  return (
-    <div className='todo-container'>
-      <h1 className='todo-container__header'>DDING TODO</h1>
-      <form onSubmit={handleSubmit} className='todo-container__form'>
-        <input 
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type="text" 
-          className="todo-container__input" 
-          placeholder="할 일을 입력해주세요." 
-          required 
-        />
-        <button type="submit" className="todo-container__button">할 일 추가</button>
-      </form>
-      <div className="render-container">
-            <div className="render-container__section">
-                <h2 className="render-container__title">할 일</h2>
-                <ul className="render-container__list">
-                {todos.map((todo) => (
-                    <li key={todo.id} className="render-container__item">
-                    <p className="render-container__item-text">{todo.text}</p>
-                    <button 
-                    onClick={():void => completeTodo(todo)}
-                    style={{
-                        backgroundColor: '#28a745'
-                    }} className="render-container__item-button">완료</button>
-                 </li>
-                ))}
-                </ul>
-            </div>
-            <div className="render-container__section">
-                <h2 className="render-container__title">완료</h2>
-                <ul className="render-container__list">
-                {doneTodos.map((todo) => (
-                    <li key={todo.id} className="render-container__item">
-                        <p className="render-container__item-text">{todo.text}</p>
-                        <button 
-                        onClick={():void => deleteTodo(todo)}
-                        style={{
-                            backgroundColor: '#dc3545'
-                        }} className="render-container__item-button">삭제</button>
-                     </li>
-                ))}
-                </ul>
+const Todo = () => {
+    const { todos, completeTodo, deleteTodo, doneTodos } = useTodo();
+    
+    return (
+        <div className="todo-container">
+            <h1 className="todo-container__header">DDING TODO</h1>
+            <TodoForm />
+            <div className="render-container">
+                <TodoList 
+                    title='할 일' 
+                    todos={todos} 
+                    buttonLabel='완료' 
+                    buttonColor='#28a745'
+                    onClick={completeTodo}    
+                />
+                <TodoList 
+                    title='완료' 
+                    todos={doneTodos} 
+                    buttonLabel='삭제' 
+                    buttonColor='#dc3545' 
+                    onClick={deleteTodo}    
+                />
             </div>
         </div>
-    </div>
-  )
-}
+    )
+};
 
-export default Todo
+export default Todo;
