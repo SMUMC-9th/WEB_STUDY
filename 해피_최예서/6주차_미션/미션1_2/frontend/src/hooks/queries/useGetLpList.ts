ece59fd4f1b92e79c0f2a9080766295eb1f8a31e
choiyeseo <127025299+choiyeseo@users.noreply.github.com>
2025-11-03 17:13:43 +0900

    W5&6 과제 - 해피/최예서 (#49)

---
+++ 해피_최예서/6주차_미션/미션1_2/frontend/src/hooks/queries/useGetLpList.ts
@@ -0,0 +1,26 @@
+// useGetLpList.ts
+import { useQuery } from "@tanstack/react-query";
+import type { PaginationDto } from "../../types/common.ts";
+import { getLpList } from "../../apis/lp.ts";
+import { QUERY_KEY } from "../../constants/key.ts";
+
+function useGetLpList({ cursor, search, order, limit }: PaginationDto) {
+  return useQuery({
+    queryKey: [QUERY_KEY.lps, search, order, cursor, limit],
+    queryFn: () =>
+      getLpList({
+        cursor,
+        search,
+        order,
+        limit,
+      }),
+
+    // 캐싱
+    staleTime: 1000 * 60 * 5, // 5m
+    gcTime: 100 * 60 * 10, // 10m
+
+    select: (data) => data.data.data,
+  });
+}
+
+export default useGetLpList;
