af467f7df0d114b48a0c0259c8c5124a3c4311a9
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2025-11-24 17:08:31 +0900

    FEAT: W9 미션2 Modal Slice

--- 유니_이혜윤/9주차_미션/playList/src/components/TotalPrice.tsx
+++ 유니_이혜윤/9주차_미션/playList/src/components/TotalPrice.tsx
@@ -1,23 +1,43 @@
 import { useAppDispatch, useAppSelector } from "../hooks/useCustomRedux";
 import { clearCart } from "../slices/cartSlice";
+import { openModal, closeModal } from "../slices/modalSlice";
+import Modal from "./Modal";
 
 const TotalPrice = () => {
   const { total } = useAppSelector((state) => state.cart);
+  const { isOpen } = useAppSelector((state) => state.modal);
   const dispatch = useAppDispatch();
 
-  const handleClearCart = () => {
+  const handleOpenModal = () => {
+    dispatch(openModal());
+  };
+
+  const handleConfirm = () => {
     dispatch(clearCart());
+    dispatch(closeModal());
+  };
+
+  const handleCancel = () => {
+    dispatch(closeModal());
   };
 
   return (
     <div className="flex justify-between items-center px-10 py-4 max-w-5xl mx-auto">
       <button
-        onClick={handleClearCart}
-        className="bg-indigo-100 px-3 py-1 rounded cursor-pointer"
+        onClick={handleOpenModal}
+        className="bg-red-100 text-sm text-gray-600 px-3 py-1.5 rounded cursor-pointer"
       >
         장바구니 초기화
       </button>
       <div>총 가격 : {total}원</div>
+      <Modal
+        isOpen={isOpen}
+        title="장바구니를 초기화하시겠습니까?"
+        confirmText="확인"
+        cancelText="취소"
+        onConfirm={handleConfirm}
+        onCancel={handleCancel}
+      />
     </div>
   );
 };
