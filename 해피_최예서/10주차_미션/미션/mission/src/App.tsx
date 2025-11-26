563384fedee6b318dd4227426dd638a184e6fbb3
choiyeseo <yeeseoy@gmail.com>
2025-11-26 20:18:54 +0900

    add mission2

--- 해피_최예서/10주차_미션/미션/mission/src/App.tsx
+++ 해피_최예서/10주차_미션/미션/mission/src/App.tsx
@@ -1,13 +1,28 @@
 import "./App.css";
 import HomePage from "./pages/HomePage.tsx";
 import { MovieProvider } from "./Context/MovieContext.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MovieDetailPage from "./pages/MovieDetailPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/:id",
+    element: <MovieDetailPage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <MovieProvider>
-        <HomePage />
-      </MovieProvider>
+      <div className="App">
+        <MovieProvider>
+          <RouterProvider router={router} />
+        </MovieProvider>
+      </div>
     </>
   );
 }
